94b5c88e98eb6be97558ff94e3433decef6adb2b
Domingo Ibrahin Pulgar <domingo0208@gmail.com>
2022-11-23 22:16:01 -0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,22 @@
+const displayValorAnterior = document.getElementById('valor-anterior');
+const displayValorActual = document.getElementById('valor-actual');
+const botonesNumeros = document.querySelectorAll('.numero');
+const botonesOperadores = document.querySelectorAll('.operador');
+
+const display = new Display(displayValorAnterior, displayValorActual);
+
+function displayNone() {
+    document.getElementById("displayuser").style.display="none";
+ }
+
+function displayFlex() {
+    document.getElementById("displayuser").style.display="flex";
+}
+
+botonesNumeros.forEach(boton => {
+    boton.addEventListener('click', () => display.agregarNumero(boton.innerHTML));
+});
+
+botonesOperadores.forEach(boton => {
+    boton.addEventListener('click', () => display.computar(boton.value));
+});
